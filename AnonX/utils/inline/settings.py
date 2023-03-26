59826830bcc5ed3ea704858937a9ad3f25056cc4
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-03-26 23:07:54 +0530

    Update settings.py

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -8,18 +8,18 @@ def setting_markup(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ", callback_data="AQ"
+                text="🤪ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ", callback_data="AQ"
             ),
             InlineKeyboardButton(
-                text="ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ", callback_data="VQ"
+                text="😘ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ", callback_data="VQ"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="ᴀᴜᴛʜ ᴜsᴇʀs", callback_data="AU"
+                text="😂ᴀᴜᴛʜ ᴜsᴇʀs", callback_data="AU"
             ),
             InlineKeyboardButton(
-                text="ᴄʟᴇᴀɴ ᴍᴏᴅᴇ", callback_data="CM"
+                text="🙄ᴄʟᴇᴀɴ ᴍᴏᴅᴇ", callback_data="CM"
             ),
         ],
         [
